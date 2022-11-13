976b9a0ee746769e2a06f4753f829b1dc2361341
roman.zubriichuk <rzubriichuk@incomm.com>
2022-11-14 01:40:09 +0200

    feat(material): add material

---
+++ src/app/modules/material/AppMaterialModule.ts
@@ -0,0 +1,11 @@
+import {NgModule} from "@angular/core";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+
+@NgModule({
+  imports: [MatTableModule, MatPaginatorModule],
+  exports: [MatTableModule, MatPaginatorModule]
+})
+export class AppMaterialModule {
+
+}
